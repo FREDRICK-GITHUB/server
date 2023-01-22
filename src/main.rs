f8cd5512cb7389be8f854838ee2da394f933bc25
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-01-22 21:46:21 +0300

    setup the server initialization point

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod http;
+mod server;
+
 fn main() {
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run();
     
 }
 
-struct Server {
-    addr: String,
-}
 
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr: addr }
-    }
-
-    fn run(self){
-        println!("Listening on {}", self.addr); 
-    }
-}
