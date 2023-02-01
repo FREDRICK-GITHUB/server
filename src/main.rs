11af74d1b9f18763829ce1fd97aac45f4308e747
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-02-01 08:36:48 +0300

    added handler and handler implementation

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@
 use server::Server;
 use http::Request;
 use http::Method;
+use website_handler::WebsiteHandler;
 
 mod http;
 mod server;
+mod website_handler;
 
 fn main() {
     let server = Server::new(String::from("127.0.0.1:8080"));
-    server.run();
+    server.run(WebsiteHandler);
     
 }
 
